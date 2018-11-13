099b37bdba14aa35d9be3855d5f6ad25128ae885
Mayisa Sanchita <38496281+Mayisa-Sanchita@users.noreply.github.com>
2018-11-13 11:09:34 +0600

    Add files via upload

---
+++ LambdaFunction/src/main/HalloWorld.java
@@ -0,0 +1,5 @@
+package main;
+
+public interface HalloWorld {
+   public String sayHello(String name);
+}
---
+++ LambdaFunction/src/main/HalloWorldRun.java
@@ -0,0 +1,10 @@
+package main;
+
+public class HalloWorldRun {
+	 public static void main(String[] args)
+	  {
+		  HalloWorld hw = (name) -> "Hello " + name;
+		  System.out.println(hw.sayHello("SWAZAN"));
+		  
+	  }
+}
